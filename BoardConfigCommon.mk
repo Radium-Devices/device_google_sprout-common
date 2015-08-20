517f7cd6fd2ab77eed357182934fb36ea75d9e52
randomlyjobless&bored <randomlybored@openmailbox.org>
2015-08-20 22:36:41 +0530

    RadiumIfy
    Change-Id: I2a3318204ffdfc8ccc11d8191dd4658d1ee993f3
    Signed-off-by: randomlyjobless&bored <randomlybored@openmailbox.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -21,12 +21,13 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 # kernel stuff
-TARGET_KERNEL_SOURCE := kernel/mediatek/sprout
-TARGET_KERNEL_CONFIG := cyanogenmod_sprout_defconfig
+#TARGET_KERNEL_SOURCE := kernel/mediatek/sprout
+#TARGET_KERNEL_CONFIG := cyanogenmod_sprout_defconfig
 BOARD_KERNEL_CMDLINE :=
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x04000000 --tags_offset 0x00000100
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
+TARGET_PREBUILT_KERNEL := device/google/sprout-common/kernel
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
